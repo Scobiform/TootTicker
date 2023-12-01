a423f2149278583e478fcdb04523401204364c80
Uwe Siemer <uwe@scobiform.com>
2023-12-01 15:55:52 +0100

    added save account info to json

--- TootTicker.py
+++ TootTicker.py
@@ -1,7 +1,10 @@
-# TootTicker - boost your media and journalists
 from mastodon import Mastodon
 import json
 import time
+
+# TootTicker - boost your media and journalists
+# This script will fetch the account information from the provided Mastodon URLs
+# and save the information to a JSON file
 
 def create_secrets():
     # Replace the following placeholders with your actual values
@@ -37,7 +40,9 @@ with open('mastodon_urls.json', 'r') as file:
 # Create Mastodon API instance
 mastodon = Mastodon(access_token='usercred.secret')
 
-def retweet_from_all_accounts():
+def get_account_infos():
+    account_info_list = []  # List to store account information
+
     for url in mastodon_urls:
         try:
             # Resolve the profile URL to get the account details
@@ -49,25 +54,39 @@ def retweet_from_all_accounts():
                 # Get recent toots from the user's timeline
                 toots = mastodon.account_statuses(user_id, limit=5)
 
+                # Create a dictionary with account information
+                account_info = {
+                    "Account Name": account[0]['username'],
+                    "Account URL": account[0]['url'],
+                    "Account ID": user_id,
+                    "Followers": account[0]['followers_count'],
+                    "Following": account[0]['following_count'],
+                    "Toots": account[0]['statuses_count'],
+                    "Created": account[0]['created_at'],
+                    "Last Active": account[0]['last_status_at'],
+                    "Bot": account[0]['bot']
+                }
+
                 # Print account information from the mastodon user
-                print(f"Account Name: {account[0]['username']}")
-                print(f"Account URL: {account[0]['url']}")
-                print(f"Account ID: {user_id}")
-                # Print Followers and Following
-                print(f"Followers: {account[0]['followers_count']}")
-                print(f"Following: {account[0]['following_count']}")
-                # Print the number of toots
-                print(f"Toots: {account[0]['statuses_count']}")
-                print(f"Created: {account[0]['created_at']}")
-                print(f"Last Active: {account[0]['last_status_at']}")
-                print(f"Bot: {account[0]['bot']}")
+                for key, value in account_info.items():
+                    print(f"{key}: {value}")
+
+                # Append the account_info dictionary to the list
+                account_info_list.append(account_info)
+
+                # Rate limiting
+                time.sleep(5)
 
         except Exception as e:
             print(f"Error processing {url}: {e}")
+
+    # Save the account information to a JSON file
+    with open('account_info.json', 'w') as json_file:
+        json.dump(account_info_list, json_file, indent=2)
 
 def main():
     # Who Am I
     print(mastodon.me().username)
 
 if __name__ == '__main__':
-    retweet_from_all_accounts()
+    get_account_infos()
