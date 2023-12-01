714023972c59a912dc1bff6406c739c61dba53ee
Uwe Siemer <uwe@scobiform.com>
2023-12-01 19:11:54 +0100

    feat: adding worker service and basic html view

--- TootTicker.py
+++ TootTicker.py
@@ -1,6 +1,9 @@
+import tracemalloc
 from mastodon import Mastodon
+import os
 import json
 import time
+from threading import Thread
 
 # TootTicker - boost your media and journalists
 # Gathering account informations from Mastodon and make them available as pure json files
@@ -42,7 +45,7 @@ mastodon = Mastodon(access_token='usercred.secret')
 
 import os
 
-def get_account_infos():
+def get_account_infos(mastodon):
     for url in mastodon_urls:
         try:
             # Resolve the profile URL to get the account details
@@ -82,15 +85,86 @@ def get_account_infos():
                 with open(os.path.join(accounts_directory, str(user_id) + '.json'), 'w') as file:
                     json.dump(account_info, file, indent=4, default=str)
 
-                # Rate limiting
-                time.sleep(2)
-
         except Exception as e:
             print(f"Error processing {url}: {e}")
 
+def generate_html_overview():
+    # Define the output HTML file
+    output_file = 'account_overview.html'
+
+    # Get the list of JSON files in the 'accounts/' folder
+    json_files = [f for f in os.listdir('accounts/') if f.endswith('.json')]
+
+    # Open the HTML file for writing
+    with open(output_file, 'w') as html_file:
+        # Write the HTML header
+        html_file.write('<html>\n<head>\n<title>Account Overview</title>\n</head>\n<body>\n')
+
+        # Write the table header
+        html_file.write('<table border="1">\n<tr>\n<th>File Name</th>\n<th>Contents</th>\n</tr>\n')
+
+        # Iterate through each JSON file
+        for json_file in json_files:
+            # Read the contents of the JSON file
+            with open(f'accounts/{json_file}', 'r') as file:
+                try:
+                    # Attempt to load JSON content
+                    json_content = json.load(file)
+                except json.decoder.JSONDecodeError as e:
+                    # Handle JSON decoding error (e.g., empty file or invalid JSON)
+                    print(f"Error decoding {json_file}: {e}")
+                    continue
+
+            # Write a table row with the file name and contents
+            html_file.write(f'<tr>\n<td>{json_file}</td>\n<td><pre>{json.dumps(json_content, indent=2)}</pre></td>\n</tr>\n')
+
+        # Write the HTML footer
+        html_file.write('</table>\n</body>\n</html>')
+
+    print(f'HTML overview generated in {output_file}')
+
+def worker(mastodon):
+    try:
+        # Create the UI thread
+        generateUI = Thread(target=generate_html_overview)
+
+        # Create account gathering thread
+        accountInfos = Thread(target=get_account_infos, args=(mastodon,))
+
+        # Create a list of threads
+        threads = []
+
+        # Start the UI thread
+        threads.append(generateUI)
+        # Start the account gathering thread
+        threads.append(accountInfos)
+
+        # Start all threads
+        for j in threads:
+            j.start()
+
+        # Wait for all threads to complete
+        for j in threads:
+            j.join()
+
+    except Exception as errorcode:
+        print("ERROR: " + str(errorcode))
+
 def main():
+
+    # Enable tracemalloc
+    tracemalloc.start()
+    
+    # Authenticate the app
+    global mastodon
+    mastodon = Mastodon(access_token = 'usercred.secret')
+
     # Who Am I
     print(mastodon.me().username)
 
+    # Start Worker
+    worker(mastodon)
+
 if __name__ == '__main__':
-    get_account_infos()
+    main()
+input()
