f64687636cfa703b01f2671448e6f36b7512fab1
Uwe Siemer <uwe@scobiform.com>
2023-12-17 03:04:43 +0100

    feat: Charts

--- TootTicker.py
+++ TootTicker.py
@@ -299,7 +299,7 @@ def generateAccountOverview():
 
             # Write the rest of the account information
             for key, value in account_info.items():
-                if key not in ["Account Name", "Avatar", "Header", "Toots", "Account URL", "Display Name", "Instance", "Account ID"]:
+                if key not in ["Account Name", "Avatar", "Header", "TootsList", "Account URL", "Display Name", "Instance", "Account ID"]:
                     html_content += f'<p><strong>{key}:</strong> {value}</p>\n'
 
             # Write the Toots header
@@ -473,10 +473,10 @@ def worker(mastodon):
             threads = []
 
             # Iterate through each category and start a thread for each
-            for category, urls in data.items():
-                # Create account gathering thread for each category
-                accountInfos = Thread(target=saveAccountInfoToJSON, args=(mastodon, category, urls))
-                threads.append(accountInfos)
+            # for category, urls in data.items():
+            #     # Create account gathering thread for each category
+            #     accountInfos = Thread(target=saveAccountInfoToJSON, args=(mastodon, category, urls))
+            #     threads.append(accountInfos)
             
             # Create HTML overview thread
             htmlOverview = Thread(target=generateAccountOverview)
