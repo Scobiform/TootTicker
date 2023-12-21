ccd2752550360832a7880e863fa70b4174624fd1
Uwe Siemer <uwe@scobiform.com>
2023-12-22 00:58:53 +0100

    License

--- TootTicker.py
+++ TootTicker.py
@@ -11,7 +11,7 @@ from threading import Thread
 # github.com/Scobiform/TootTicker
 
 # THe following libraries are used in this project:
-# Mastodon.py - MIT license - https://github.com/halcy/Mastodon.py
+# Mastodon.py - MIT License - https://github.com/halcy/Mastodon.py
 # Chart.js - MIT License - https://www.chartjs.org/
 
 # You can see a demo of this project here: https://tootticker.scobiform.com/
